1f86c46ea3f496cd7cb0bad90244ec701934982c
Snipcola <snipcola@gmail.com>
2022-12-22 21:44:32 +0000

    email

--- jobs/ip_servers.js
+++ jobs/ip_servers.js
@@ -3,21 +3,40 @@ const ping = require('ping');
 const { tables } = require('../lib/mysql/queries');
 const { selectInTable, updateInTable } = require('../lib/mysql/functions');
 
+const { emails: { statusChanged } } = require('../lib/sendgrid/config');
+const { sendEmail } = require('../lib/sendgrid/functions');
+
+const { plans } = require('../lib/config');
+
 module.exports = {
     execute: async () => {
-        const { data: { rows: servers } } = await selectInTable(tables.ipServers, 'id,ip_address,status');
+        const { data: { rows: servers } } = await selectInTable(tables.ipServers, 'id,nickname,owner_id,ip_address,status');
 
-        servers?.forEach(({ id, ip_address, status }) => ping.promise.probe(ip_address, { timeout: 10 })
+        servers?.forEach(({ id, ip_address, status, nickname, owner_id }) => ping.promise.probe(ip_address, { timeout: 10 })
             .then(async (res) => {
                 const reachable = res?.alive ?? false;
                 const new_status = reachable ? 'ONLINE' : 'OFFLINE';
 
-                if (status !== new_status) await updateInTable(tables.ipServers, [
-                    { name: 'status', value: new_status }
-                ], [
-                    { name: 'id', value: id, seperator: 'AND' },
-                    { name: 'ip_address', value: ip_address }
-                ]);
+                if (status !== new_status) {
+                    await updateInTable(tables.ipServers, [
+                        { name: 'status', value: new_status }
+                    ], [
+                        { name: 'id', value: id, seperator: 'AND' },
+                        { name: 'ip_address', value: ip_address }
+                    ]);
+                    
+                    const { data: { rows: users } } = await selectInTable(tables.users, 'email,username,subscription', [
+                        { name: 'id', value: owner_id }
+                    ]);
+
+                    const { email, username, subscription } = users[0];
+
+                    if (plans.filter((p) => p !== 'FREE').includes(subscription)) {
+                        const statusEmail = statusChanged(email, username, nickname, status, new_status);
+                    
+                        await sendEmail(statusEmail);
+                    };
+                };
             })
             .catch(() => false));
     },
